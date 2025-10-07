b8eb724e2fe52ea9865449a7699c845b186defa2
Oleg <olegka35@gmail.com>
2025-10-07 20:19:37 +0400

    Gradle spring web app set up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,20 @@
 plugins {
     id("java")
+    id("war")
 }
 
 group = "com.tarasov"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation(platform("org.springframework:spring-framework-bom:6.2.11"))
+    implementation("org.springframework:spring-context")
+    implementation("org.springframework:spring-aspects")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
