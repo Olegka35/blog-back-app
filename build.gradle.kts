149e7fe17cff758d5be00744227aa4c017206381
Oleg <olegka35@gmail.com>
2025-10-07 21:12:24 +0400

    DispatcherServlet configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,9 @@ repositories {
 
 dependencies {
     implementation(platform("org.springframework:spring-framework-bom:6.2.11"))
-    implementation("org.springframework:spring-context")
+    implementation("org.springframework:spring-webmvc")
     implementation("org.springframework:spring-aspects")
+    compileOnly("jakarta.servlet:jakarta.servlet-api:6.1.0")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
