26bedbb3bc10f96f7df57b86ff7a070cf54ddf83
Oleg <olegka35@gmail.com>
2025-10-12 02:32:28 +0300

    Logging

--- src/main/java/com/tarasov/configuration/WebConfiguration.java
+++ src/main/java/com/tarasov/configuration/WebConfiguration.java
@@ -3,6 +3,7 @@ package com.tarasov.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -10,5 +11,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @ComponentScan(basePackages = "com.tarasov")
 @PropertySource("classpath:application.properties")
+@EnableAspectJAutoProxy
 public class WebConfiguration {
 }
--- src/main/java/com/tarasov/controller/advice/ControllerExceptionHandler.java
+++ src/main/java/com/tarasov/controller/advice/ControllerExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.tarasov.controller.advice;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -16,14 +18,18 @@ import java.util.Map;
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<String> handleResponseStatus(ResponseStatusException ex) {
+        LOGGER.error(ex.getMessage(), ex);
         return ResponseEntity.status(ex.getStatusCode()).body(ex.getReason());
     }
 
     @ExceptionHandler({ MissingServletRequestParameterException.class, IllegalArgumentException.class })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, Object> handleMissedParameterException(Exception exception) {
+        LOGGER.error(exception.getMessage(), exception);
         return Map.of("message", exception.getMessage() != null ? exception.getMessage() : "Unexpected error",
                 "timestamp", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH24:mm:ss")));
     }
@@ -31,6 +37,7 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String, Object> handleException(Exception exception) {
+        LOGGER.error(exception.getMessage(), exception);
         return Map.of("error", exception.getClass().getName(),
                 "message", exception.getMessage() != null ? exception.getMessage() : "Unexpected error",
                 "timestamp", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH24:mm:ss")));
